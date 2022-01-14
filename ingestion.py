8f2028658d890bb3217d5397e6b1a153cac7b3cc
Rob Smith <robsmith155@gmail.com>
2022-01-14 12:24:18 +0300

    feat: complete ingestion.py script
    This was completed to automatically read all input files and compile into one dataset.

--- ingestion.py
+++ ingestion.py
@@ -5,9 +5,7 @@ import json
 from datetime import datetime
 
 
-
-
-#############Load config.json and get input and output paths
+############ Load config.json and get input and output paths ##############
 with open('config.json','r') as f:
     config = json.load(f) 
 
@@ -15,10 +13,41 @@ input_folder_path = config['input_folder_path']
 output_folder_path = config['output_folder_path']
 
 
+################### Function for data ingestion ###########################
+def merge_multiple_dataframe() -> None:
+    """
+    Checks for datasets stored as .csv files, compiles them together
+    and then outputs the compiled data as a .csv.
+    """
+    final_df = pd.DataFrame(
+        columns=[ 
+            'lastmonth_activity', 
+            'lastyear_activity', 
+            'number_of_employees', 
+            'exited'])
 
-#############Function for data ingestion
-def merge_multiple_dataframe():
-    #check for datasets, compile them together, and write to an output file
+    filenames = os.listdir(input_folder_path)
+    csv_files = []
+    
+    for file in filenames:
+        if file.endswith('.csv'):
+            csv_files.append(file)
+            filepath = os.path.join(input_folder_path, file)
+            df = pd.read_csv(filepath, index_col='corporation')
+            final_df = final_df.append(df)
+        else:
+            pass
+    
+    # Drop dulplicate rows
+    final_df.drop_duplicates(inplace=True)
+
+    # Save data
+    ingested_filepath = os.path.join(output_folder_path, 'ingestedfiles.txt')
+    with open(ingested_filepath, 'w') as f:
+        f.write(json.dumps(csv_files))
+    
+    data_filepath = os.path.join(output_folder_path, 'finaldata.csv')
+    final_df.to_csv(data_filepath, index_label='corporation')
 
 
 
