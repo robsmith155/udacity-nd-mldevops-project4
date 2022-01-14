abd9f182923a2198af02b0fcfbbf2e4ff7e07705
Rob Smith <robsmith155@gmail.com>
2022-01-14 14:30:52 +0300

    feat: complete deployment.py script

--- deployment.py
+++ deployment.py
@@ -1,27 +1,35 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 import json
-
-
+import shutil
 
 ##################Load config.json and correct path variable
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
+ingested_files_path = os.path.join(
+    config['output_folder_path'], 'ingestedfiles.txt'
+    )
+model_path = os.path.join(config['output_model_path'], 'trainedmodel.pkl')  
+score_path = os.path.join(config['output_model_path'], 'latestscore.txt')
+files_to_copy = [ingested_files_path, model_path, score_path]
 
+prod_deployment_path = config['prod_deployment_path']
 
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
+#################### Function for deployment #############################
+def copy_files_to_deployment() -> None:
+    """
+    Function to copy latest model, model score and ingest data info to
+    the deployment folder.
+    """
+    
+    for file in files_to_copy:
+        if os.path.isfile(file):
+            shutil.copy(
+                src=file,
+                dst = os.path.join(prod_deployment_path, file.split('/')[-1])
+                )  
+    
+
+if __name__ == '__main__':
+    copy_files_to_deployment()    
 
