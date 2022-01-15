3b14aa8fa841d86e758650d6691e95d333944396
Rob Smith <robsmith155@gmail.com>
2022-01-15 12:43:05 +0300

    feat: add predictions and summary statistics to diagnostics.py

--- diagnostics.py
+++ diagnostics.py
@@ -1,42 +1,78 @@
-
 import pandas as pd
 import numpy as np
 import timeit
 import os
 import json
+import pickle
+
+from utils import load_data
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv') 
+test_data_path = os.path.join(config['test_data_path'], 'testdata.csv') 
+model_path = os.path.join(config['prod_deployment_path'], 'trainedmodel.pkl') 
 
 ##################Function to get model predictions
-def model_predictions():
-    #read the deployed model and a test dataset, calculate predictions
-    return #return value should be a list containing all predictions
+def model_predictions(data_path: str) -> list:
+    """
+    Function to compute predictions on data in data_path.
+
+    Inputs
+    ------
+    data_path : str
+        Path to dataset to run predictions on.
+    Returns
+    -------
+    y_pred : list
+        Model predictions.
+    """
+    X, y = load_data(data_path = data_path)
+
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+
+    y_pred = model.predict(X)
+    return y_pred.tolist()
 
 ##################Function to get summary statistics
-def dataframe_summary():
-    #calculate summary statistics here
-    return #return value should be a list containing all summary statistics
+def dataframe_summary(data_path: str) -> dict:
+    """
+    Function to calculate summary statistics on the training dataset.
+
+    Inputs
+    ------
+    data_path : str
+        Filepath of dataset to summarize.
+    Returns
+    -------
+    summary_dict : dict
+        Dictionary of the dataset summary statistics.
+    """
+    df = pd.read_csv(dataset_csv_path, index_col='corporation')
+    summary_dict = {}
+    summary_dict['col_means'] = dict(df.mean())
+    summary_dict['col_medians'] = dict(df.median())
+    summary_dict['col_std'] = dict(df.std())
+    return summary_dict
 
 ##################Function to get timings
-def execution_time():
+#def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    #return #return a list of 2 timing values in seconds
 
 ##################Function to check dependencies
-def outdated_packages_list():
+#def outdated_packages_list():
     #get a list of 
 
 
 if __name__ == '__main__':
-    model_predictions()
-    dataframe_summary()
-    execution_time()
-    outdated_packages_list()
+    model_predictions(data_path = test_data_path)
+    dataframe_summary(data_path = dataset_csv_path)
+    #execution_time()
+    #outdated_packages_list()
 
 
 
--- utils.py
+++ utils.py
@@ -5,6 +5,22 @@ import numpy as np
 
 
 def load_data(data_path: str) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Function to load a csv file in data_path to a Pandas DataFrame.
+    The data is then split into inputs and target variable and 
+    returned as Numpy arrays.
+
+    Inputs
+    ------
+    data_path : str
+        Filepath of the data to load.
+    Returns
+    -------
+    X : np.ndarray
+        Input features.
+    y : np.ndarray
+        Target variable.
+    """
     df = pd.read_csv(data_path, index_col='corporation')
     y = df['exited'].values.reshape(-1,1).ravel()
     X = df.drop(columns='exited').values.reshape(-1,3)
