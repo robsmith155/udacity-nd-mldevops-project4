bf2a2e8e1a827c42bde8dd9c300841f74ffd0d74
Rob Smith <robsmith155@gmail.com>
2022-01-15 17:14:51 +0300

    feat: add flask app to serve model and information

--- apicalls.py
+++ apicalls.py
@@ -1,20 +1,33 @@
+import json
+import os
+
 import requests
 
+# Load config
+with open('config.json','r') as f:
+    config = json.load(f) 
+
+output_path = os.path.join(config['output_model_path'], 'apireturns.txt')
+
 #Specify a URL that resolves to your workspace
-URL = "http://127.0.0.1/"
-
-
+URL = "http://127.0.0.1:8000"
 
 #Call each API endpoint and store the responses
-response1 = #put an API call here
-response2 = #put an API call here
-response3 = #put an API call here
-response4 = #put an API call here
+response1 = requests.post(f'{URL}/prediction?datapath=testdata/testdata.csv').content
+response2 = requests.get(f'{URL}/scoring').content
+response3 = requests.get(f'{URL}/summarystats?datapath=testdata/testdata.csv').content
+response4 = requests.get(f'{URL}/diagnostics?datapath=testdata/testdata.csv').content
 
 #combine all API responses
-responses = #combine reponses here
+responses = {
+    'Prediction response': json.loads(response1.decode('utf8').replace("'", '"')),
+    'Scoring response': json.loads(response2.decode('utf8').replace("'", '"')),
+    'Summary stats response': json.loads(response3.decode('utf8').replace("'", '"')),
+    'Diagnostics response': json.loads(response4.decode('utf8').replace("'", '"'))
+}
 
 #write the responses to your workspace
-
+with open(output_path, 'w') as f:
+    f.write(json.dumps(responses))
 
 
--- app.py
+++ app.py
@@ -1,50 +1,71 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
-import json
-import os
+from flask import Flask, request
 
+from diagnostics import (
+    model_predictions,
+    dataframe_summary, 
+    missing_data, 
+    execution_time, 
+    outdated_packages_list)
+from scoring import score_model
 
 
 ######################Set up variables for use in our script
 app = Flask(__name__)
 app.secret_key = '1652d576-484a-49fd-913a-6879acfa6ba4'
 
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
+########################### Prediction Endpoint #############################
 
-prediction_model = None
-
-
-#######################Prediction Endpoint
 @app.route("/prediction", methods=['POST','OPTIONS'])
 def predict():        
-    #call the prediction function you created in Step 3
-    return #add return value for prediction outputs
+    """
+    Endpoint to return predictions from the trained model.
+    """
+    data_path = request.args.get('datapath')
+    y_preds = model_predictions(data_path=data_path)
+    return str(y_preds)
 
-#######################Scoring Endpoint
+############################ Scoring Endpoint ###############################
+
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
-    #check the score of the deployed model
-    return #add return value (a single F1 score number)
+def score():
+    """
+    Endpoint to check the score of the current model on the test dataset
+    """
+    score = score_model()
+    return str(score)
 
-#######################Summary Statistics Endpoint
+####################### Summary Statistics Endpoint #########################
+
 @app.route("/summarystats", methods=['GET','OPTIONS'])
-def stats():        
-    #check means, medians, and modes for each column
-    return #return a list of all calculated summary statistics
+def stats():
+    """
+    Endpoint to return summary statistics from a selected dataset
+    """
+    data_path = request.args.get('datapath')
+    summary = dataframe_summary(data_path=data_path)     
+    return str(summary)
 
-#######################Diagnostics Endpoint
+########################### Diagnostics Endpoint ############################
+
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
-    #check timing and percent NA values
-    return #add return value for all diagnostics
+def diagnostics():
+    """
+    Run and resturn results from diagnostic checks including execution time,
+    the percentage of missing values from each column in the dataset and 
+    a list of the packages that are not using the latest version.
+    """
+    data_path = request.args.get('datapath')
+    col_na_pc = missing_data(data_path=data_path)
+    runtimes = execution_time()
+    outdated_packages = outdated_packages_list()
+    output ={
+        'Column missing_values_pc': col_na_pc,
+        'Runtimes': runtimes,
+        'Outdated packages': outdated_packages
+        }
+    return str(output)
+
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
