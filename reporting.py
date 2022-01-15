ee72b0e16f29389e40205fa03493c36be08ed8f1
Rob Smith <robsmith155@gmail.com>
2022-01-15 15:05:35 +0300

    feat: add reporting.py script to produce confusion matrix

--- reporting.py
+++ reporting.py
@@ -1,31 +1,32 @@
-import pickle
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import numpy as np
-from sklearn import metrics
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
 import matplotlib.pyplot as plt
-import seaborn as sns
 import json
 import os
 
-
+from diagnostics import model_predictions
+from utils import load_data
 
 ###############Load config.json and get path variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-
-
+dataset_csv_path = os.path.join(config['output_folder_path'], 'finaldata.csv') 
+test_data_path = os.path.join(config['test_data_path'], 'testdata.csv') 
+conf_matrix_path = os.path.join(config['output_model_path'], 'confusionmatrix.png') 
 
 
 ##############Function for reporting
-def score_model():
-    #calculate a confusion matrix using the test data and the deployed model
-    #write the confusion matrix to the workspace
+def score_model() -> None:
+    """
+    Outputs a confusion matrix for the test dataset.
+    """
+    y_preds = model_predictions(data_path=test_data_path)
+    _, y_true = load_data(data_path=test_data_path)
 
-
-
+    cm = confusion_matrix(y_true, y_preds)
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm)
+    disp.plot()
+    plt.savefig(conf_matrix_path)
 
 
 if __name__ == '__main__':
